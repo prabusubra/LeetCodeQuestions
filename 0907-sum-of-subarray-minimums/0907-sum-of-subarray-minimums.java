eba168ee04413e343ec19b3aa5e1d2eda4b57922
Prabu Subra <prabus69@gmail.com>
2024-04-06 16:36:32 +0530

    Time: 20 ms (88.64%), Space: 49 MB (85.50%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,48 +1,36 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-    int m = 1_000_000_007;
-    int[] lse = new int[arr.length];
-    int[] rse = new int[arr.length];
+        final int kMod = 1_000_000_007;
+    final int n = arr.length;
+    long ans = 0;
+    // prevMin[i] := index k s.t. arr[k] is the previous minimum in arr[:i]
+    int[] prevMin = new int[n];
+    // nextMin[i] := index k s.t. arr[k] is the next minimum in arr[i + 1:]
+    int[] nextMin = new int[n];
+    Deque<Integer> stack = new ArrayDeque<>();
 
-    Arrays.fill(lse, -1);
-    Arrays.fill(rse, arr.length);
+    Arrays.fill(prevMin, -1);
+    Arrays.fill(nextMin, n);
 
-    Stack<Integer> stack = new Stack<>();
     stack.push(0);
-
-    for (int i = 1; i < lse.length; i++) {
-        //find minimun number index from stack and set it in lse array for ith value.
-        while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-            stack.pop();
-        }
-
-        if (!stack.isEmpty()){
-            lse[i] = stack.peek();
-        }
-
-        stack.push(i);
+    for (int i = 1; i < arr.length; ++i) {
+      while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+        nextMin[stack.pop()] = i;
+      }
+      if (!stack.isEmpty()){
+        prevMin[i] = stack.peek();
+      }
+      stack.push(i);
     }
 
-    stack.clear();
-    stack.push(arr.length-1);
-    for (int i = arr.length-2; i >=0; i--) {
-        //find minimun number index from stack and set it in lse array for ith value.
-        while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-            stack.pop();
-        }
-        if (!stack.isEmpty()){
-            rse[i] = stack.peek();
-        }
+    //System.out.println("Prev Min : "+Arrays.toString(prevMin));
+    //System.out.println("Next Min : "+Arrays.toString(nextMin));
 
-        stack.push(i);
-    }
-    long sum=0;
-    for (int i = 0; i < arr.length; i++) {
-        long leftRange = i-lse[i];
-        int rightRange = rse[i] - i;
-        sum = sum%m + leftRange%m*rightRange%m*arr[i]%m;
+    for (int i = 0; i < arr.length; ++i) {
+      ans += (long) arr[i] * (i - prevMin[i]) * (nextMin[i] - i);
+      ans %= kMod;
     }
 
-    return (int)sum;
+    return (int) ans;
     }
 }
