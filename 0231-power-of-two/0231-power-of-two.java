7cfdab8a4f96ca709ebd775b711d8dc46daab975
Prabu Subra <prabus69@gmail.com>
2024-05-15 22:06:36 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (56.63%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <1) return false;
+        int count = 0;
+        while (n != 0){
+            n = n & n-1;
+            count+=1;
+            if (count>1) return false;
+        }
+        return true;
+    }
+}
