05bbc671626102d10f78d54adf3d8472ef1e4415
Prabu Subra <prabus69@gmail.com>
2025-01-02 23:25:33 +0530

    Time: 2 ms (92.38%), Space: 42.1 MB (85%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -2,40 +2,39 @@ import java.util.*;
 
 class Solution {
     public String removeDuplicateLetters(String s) {
-        // Track the last occurrence of each character
+        
         int[] lastIndex = new int[26];
-        for (int i = 0; i < s.length(); i++) {
+
+        for (int i=0; i < s.length(); i++) {
             lastIndex[s.charAt(i) - 'a'] = i;
         }
 
-        // Use a stack to build the result
         Stack<Character> stack = new Stack<>();
-        // Visited array to track if a character is in the stack
+
         boolean[] visited = new boolean[26];
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            // If the character is already in the stack, skip it
-            if (visited[ch - 'a']) continue;
+        for (int i=0; i < s.length(); i++) {
+            char curr = s.charAt(i);
 
-            // Remove characters from the stack that are:
-            // - greater than the current character
-            // - can appear later (i.e., their last occurrence is after the current index)
-            while (!stack.isEmpty() && stack.peek() > ch && lastIndex[stack.peek() - 'a'] > i) {
-                visited[stack.pop() - 'a'] = false;
+            if (!visited[curr - 'a']) {
+
+                while (!stack.isEmpty() &&
+                        stack.peek() > curr &&
+                        lastIndex[stack.peek() - 'a'] > i) {
+                            visited[stack.pop() - 'a'] = false;
+                        }
+                
+                stack.push(curr);
+                visited[curr - 'a'] = true;
+
             }
-
-            // Push the current character onto the stack
-            stack.push(ch);
-            visited[ch - 'a'] = true;
         }
 
-        // Build the result from the stack
-        StringBuilder result = new StringBuilder();
-        for (char ch : stack) {
-            result.append(ch);
+        StringBuilder builder = new StringBuilder();
+        while (!stack.isEmpty()) {
+            builder.append(stack.pop());
         }
 
-        return result.toString();
+        return builder.reverse().toString();
     }
 }
