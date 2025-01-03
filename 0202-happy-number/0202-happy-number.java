344707d579b40cc38e8faf8394ccd7db10129511
Prabu Subra <prabus69@gmail.com>
2025-01-04 00:23:12 +0530

    Time: 1 ms (72.03%), Space: 40.6 MB (79.71%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        Set<Integer> visited = new HashSet<>();
+
+        while (n != 1 && !visited.contains(n)) {
+            visited.add(n);
+            n = getNext(n);
+        }
+
+        return n == 1;
+    }
+
+    public static int getNext(int n) {
+
+        int sum = 0;
+
+        while (n > 0) {
+
+            int digit = n % 10;
+
+            sum += digit * digit;
+
+            n /= 10;
+        } 
+
+        return sum;
+    }
+}
