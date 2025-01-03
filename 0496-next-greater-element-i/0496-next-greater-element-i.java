293f8f5fbbdefea9d28646733134101f17751913
Prabu Subra <prabus69@gmail.com>
2025-01-04 03:06:42 +0530

    Time: 3 ms (92.44%), Space: 43.8 MB (53.48%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,35 +1,31 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int[] result = new int[nums1.length];
-        for (int i =0; i < nums1.length; i++ ) {
-            int index = getIndex(nums2, nums1[i]);
-            result[i] = findRightMax(nums2, index);
-        }
-        return result;
-    }
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
 
-    private static int getIndex(int[] arr, int data) {
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == data) return i;
-        }
+        for (int num: nums2) {
 
-        return 0;
-    }
-
-    private static int findRightMax(int[] arr, int start) {
-        
-        int result = -1;
-        for (int i = start +1; i < arr.length; i++ ) {
-            
-            if (arr[i] > arr[start]) {
-                result = arr[i];
-                break;
+            while (!stack.isEmpty() && stack.peek() < num ) {
+                nextGreaterMap.put(stack.pop(), num);
             }
 
+            stack.push(num);
+        }
+
+        while (!stack.isEmpty()) {
+            nextGreaterMap.put(stack.pop(), -1);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++ ) {
+            result[i] = nextGreaterMap.get(nums1[i]);
         }
 
         return result;
+
+
     }
 }
