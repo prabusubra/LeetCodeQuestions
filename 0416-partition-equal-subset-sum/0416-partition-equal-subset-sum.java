f7ceb22b4ff4eef4af082aefaff44648ea2e4893
Prabu Subra <prabus69@gmail.com>
2025-01-18 02:04:26 +0530

    Time: 27 ms (78.06%), Space: 41.9 MB (93.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,36 +1,24 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+       
+       int total = 0;
 
-        for (int num: nums) sum += num;
+        for (int num: nums) total += num;
+        if (total % 2 != 0) return false;
+       int sum = total/2;
 
-        if (sum % 2 != 0) return false;
+       boolean[] dp = new boolean[sum +1];
 
-        int[][] memo = new int[nums.length][sum+1];
+        dp[0] = true;
 
-        for (int[] row: memo) {
-            Arrays.fill(row, -1);
+       for (int num: nums) {
+            for (int i = sum; i >= num; i-- ) {
+                dp[i] = dp[i] || dp[i - num];
+            }
         }
 
-        return backtrack(nums, nums.length, sum/2, memo);
+        return dp[sum];
     }
 
-    private static boolean backtrack(int[] nums, int index, int sum, int[][] memo) {
-        
-        if (sum == 0) return true;
 
-        if (index == 0) return false;
-        
-        if (memo[index-1][sum] != -1) return memo[index-1][sum] == 1;
-        
-        int result = 0;
-        if (nums[index -1] > sum) {
-            return backtrack(nums, index-1, sum, memo);
-        }
-
-        memo[index-1][sum] = (backtrack(nums, index-1, sum, memo) 
-        || backtrack(nums, index-1, sum- nums[index-1], memo)) ? 1:0;
-
-        return memo[index-1][sum] == 1;
-    }
 }
