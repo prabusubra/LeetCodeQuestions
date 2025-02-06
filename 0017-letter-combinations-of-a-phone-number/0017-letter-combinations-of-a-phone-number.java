05ceed2b3b59ee0ef36827e27e3e06d94991d5a8
Prabu Subra <prabus69@gmail.com>
2025-02-06 23:57:19 +0530

    Time: 0 ms (100%), Space: 41.9 MB (88.69%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+        List<String> result = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0) return result;
+
+        Map<Integer, String> mapper = new HashMap<>();
+
+        mapper.put(2, "abc");
+        mapper.put(3, "def");
+        mapper.put(4, "ghi");
+        mapper.put(5, "jkl");
+        mapper.put(6, "mno");
+        mapper.put(7, "pqrs");
+        mapper.put(8, "tuv");
+        mapper.put(9, "wxyz");
+
+        backtrack(digits, result, mapper, new StringBuilder(), 0);
+        return result;
+
+    }
+
+    private void backtrack(String digits, List<String> result, 
+                            Map<Integer, String> mapper, StringBuilder curr, int index) {
+
+        
+        if (index == digits.length()) {
+            result.add(curr.toString());
+            return;
+        }
+
+        //System.out.println("Reee : "+ch);
+        String letters = mapper.get(digits.charAt(index) - '0');
+
+        for (int i = 0; i < letters.length(); i++ ) {
+            curr.append(letters.charAt(i));
+
+            backtrack(digits, result, mapper, curr, index+1);
+
+            curr.deleteCharAt(curr.length()-1);
+        }
+
+    }
+}
